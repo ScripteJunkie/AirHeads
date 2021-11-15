8ee0dd74f66c9297bd8f1916fe0ecce31c8d4a0a
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2021-11-15 13:35:49 -0500

    Color tuning

--- src/trackOUT.py
+++ src/trackOUT.py
@@ -65,7 +65,7 @@ with dai.Device(pipeline) as device:
             # change it according to your need !
             # lower_white = np.array([133, 0, 0], dtype=np.uint8)
             # upper_white = np.array([179, 255, 115], dtype=np.uint8)
-            lower_white = np.array([2, 46, 125], dtype=np.uint8)
+            lower_white = np.array([2, 48, 125], dtype=np.uint8)
             upper_white = np.array([81, 255, 255], dtype=np.uint8)
 
             # Threshold the HSV image to get only white colors
@@ -93,7 +93,7 @@ with dai.Device(pipeline) as device:
             # tracked = framed.copy()
 
             for i in range(1, len(points)):
-                print(points[i])
+                # print(points[i])
                 if (233 < points[i][0] < 1694 and 182 < points[i][1] < 913):
                     cv2.circle(tracked, (points[i][0], points[i][1]), 2, (255, 0, 255), 2)
                     cv2.line(tracked, (points[i-1][0], points[i-1][1]), (points[i][0], points[i][1]), (0, 0, 100), 2)
@@ -101,7 +101,7 @@ with dai.Device(pipeline) as device:
             # result = cv2.convertScaleAbs(avg)
             # cv2.imshow('avg',result)
 
-            cv2.imshow('Mask',res)
+            # cv2.imshow('Mask',res)
             # cv2.imshow('mask',mask)
             cv2.imshow('Tracking',tracked)
 
